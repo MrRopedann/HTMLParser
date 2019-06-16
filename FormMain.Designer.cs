bc3dfb175aba6634b7320ff920a10011a9f547db
MrRopedann <bregnev589@gmail.com>
2019-06-16 16:18:08 +0500

    Add Parser

---
+++ FormMain.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Parser
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListTitles = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NumericStart = new System.Windows.Forms.NumericUpDown();
+            this.NumericEnd = new System.Windows.Forms.NumericUpDown();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericEnd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ListTitles
+            // 
+            this.ListTitles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListTitles.FormattingEnabled = true;
+            this.ListTitles.Location = new System.Drawing.Point(12, 12);
+            this.ListTitles.Name = "ListTitles";
+            this.ListTitles.Size = new System.Drawing.Size(549, 459);
+            this.ListTitles.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 487);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(426, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "© programm by Ropedann for Famous and discord channel: https://discord.gg/N6JpA29" +
+    "";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // NumericStart
+            // 
+            this.NumericStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.NumericStart.Location = new System.Drawing.Point(520, 147);
+            this.NumericStart.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumericStart.Name = "NumericStart";
+            this.NumericStart.Size = new System.Drawing.Size(10, 20);
+            this.NumericStart.TabIndex = 2;
+            this.NumericStart.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // NumericEnd
+            // 
+            this.NumericEnd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.NumericEnd.Location = new System.Drawing.Point(520, 243);
+            this.NumericEnd.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumericEnd.Name = "NumericEnd";
+            this.NumericEnd.Size = new System.Drawing.Size(10, 20);
+            this.NumericEnd.TabIndex = 4;
+            this.NumericEnd.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonStart.Location = new System.Drawing.Point(567, 12);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(99, 104);
+            this.ButtonStart.TabIndex = 5;
+            this.ButtonStart.Text = "Загрузить список";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 523);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListTitles);
+            this.Controls.Add(this.NumericStart);
+            this.Controls.Add(this.NumericEnd);
+            this.Name = "FormMain";
+            this.Text = "ScriptParser";
+            ((System.ComponentModel.ISupportInitialize)(this.NumericStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericEnd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListTitles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown NumericStart;
+        private System.Windows.Forms.NumericUpDown NumericEnd;
+        private System.Windows.Forms.Button ButtonStart;
+    }
+}
+
